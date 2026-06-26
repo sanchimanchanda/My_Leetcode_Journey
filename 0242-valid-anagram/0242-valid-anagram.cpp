3fdc68cef22a6a5eac5a1286be74841ff3e20abd
sanchi manchanda <157835819+sanchimanchanda@users.noreply.github.com>
2026-06-26 13:48:43 +0530

    Time: 4 ms (34.79%), Space: 9.5 MB (92.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+        sort(s.begin() , s.end());
+        sort(t.begin() , t.end());
 
-        unordered_map<char, int> counter;
-
-        for (char ch : s) {
-            counter[ch] = counter[ch] + 1;
-        }
-
-        for (char ch : t) {
-            if (counter.find(ch) == counter.end() || counter[ch] == 0) {
-                return false;
-            }
-            counter[ch] = counter[ch] - 1;
-        }
-
-        return true;
+        return s==t;
     }
 };
