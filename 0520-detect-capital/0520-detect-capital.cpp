1d4229cf02b595d9dfafc27998874bb2a8762f9a
sanchi manchanda <157835819+sanchimanchanda@users.noreply.github.com>
2026-07-02 02:35:56 +0530

    Time: 0 ms (100%), Space: 7.9 MB (97.86%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    bool check(string word , char start , char end) {
-        for (char ch : word) {
-            if (ch < start || ch > end)
-                return false;
-        }
-        return true;
-    }
-
     bool detectCapitalUse(string word) {
-        if (check(word,'A','Z') || check(word,'a','z') || check(word.substr(1),'a','z'))
+        int countCapitals = 0;
+        
+        for(char &ch : word) {
+            if(isupper(ch))
+                countCapitals++;
+        }
+        
+        if(countCapitals == 0)
             return true;
+        
+        if(countCapitals == word.length())
+            return true;
+        
+        if(countCapitals == 1 && isupper(word[0]))
+            return true;
+        
         return false;
     }
 };
