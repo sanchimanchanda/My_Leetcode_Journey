bb081d1e26405d2fea1f542c35c04783ce3884ed
sanchi manchanda <157835819+sanchimanchanda@users.noreply.github.com>
2026-07-04 06:26:15 +0530

    Time: 6 ms (11.35%), Space: 16.1 MB (5.03%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,52 +1,46 @@
 class Solution {
-    bool valid(const string& s, int start, int length) {
-        return length == 1 ||
-               (s[start] != '0' &&
-                (length < 3 || s.substr(start, length) <= "255"));
-    }
-
-    void helper(const string& s, int startIndex, vector<int>& dots,
-                vector<string>& ans) {
-        const int remainingLength = s.length() - startIndex;
-        const int remainingNumberOfIntegers = 4 - dots.size();
-
-        if (remainingLength > remainingNumberOfIntegers * 3 ||
-            remainingLength < remainingNumberOfIntegers) {
-            return;
-        }
-        if (dots.size() == 3) {
-            if (valid(s, startIndex, remainingLength)) {
-                string temp;
-                int last = 0;
-                for (int dot : dots) {
-                    temp.append(s.substr(last, dot));
-                    last += dot;
-                    temp.append(".");
-                }
-                temp.append(s.substr(startIndex));
-                ans.push_back(temp);
-            }
-            return;
-        }
-        for (int curPos = 1; curPos <= 3 && curPos <= remainingLength;
-             ++curPos) {
-            // Append a dot at the current position.
-            dots.push_back(curPos);
-            // Try making all combinations with the remaining string.
-            if (valid(s, startIndex, curPos)) {
-                helper(s, startIndex + curPos, dots, ans);
-            }
-            // Backtrack, i.e. remove the dot to try placing it at the next
-            // position.
-            dots.pop_back();
-        }
-    }
-
 public:
+    int n;
+    vector<string> result;
+
+    bool isValid(string str) {
+        if (str[0] == '0') {
+            return false;
+        }
+        int val = stoi(str);
+
+        return val <= 255;
+    }
+    void solve(string& s, int idx, int parts, string curr) {
+
+        if (idx == n && parts == 4) {
+            curr.pop_back(); // remove last dot
+                result.push_back(curr);
+
+            // result.push_back(curr.substr(0,curr.length() - 1))
+        }
+        if (idx + 1 <= n) {
+            solve(s, idx + 1, parts + 1, curr + s.substr(idx, 1) + ".");
+        }
+        if (idx + 2 <= n && isValid(s.substr(idx, 2))) {
+            solve(s, idx + 2, parts + 1, curr + s.substr(idx, 2) + ".");
+        }
+        if (idx + 3 <= n && isValid(s.substr(idx, 3))) {
+            solve(s, idx + 3, parts + 1, curr + s.substr(idx, 3) + ".");
+        }
+    }
     vector<string> restoreIpAddresses(string s) {
-        vector<int> dots;
-        vector<string> ans;
-        helper(s, 0, dots, ans);
-        return ans;
+        n = s.length();
+
+        if (n > 12) {
+            return {};
+        }
+
+        int parts = 0;
+        string curr = "";
+
+        solve(s, 0, parts, curr);
+
+        return result;
     }
 };
