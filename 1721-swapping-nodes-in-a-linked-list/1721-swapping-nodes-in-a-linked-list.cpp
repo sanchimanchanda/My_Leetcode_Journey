cca5adfdbd84040154369b7eab26970847c74e48
sanchi manchanda <157835819+sanchimanchanda@users.noreply.github.com>
2026-07-22 07:28:41 +0530

    Time: 0 ms (100%), Space: 185.2 MB (40.25%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -1,31 +1,32 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-int findLength(ListNode* head){
-        int count = 0 ;
-        while(head){
-            head = head->next;
-            count++;
-        }
-        return count;
-    }
     ListNode* swapNodes(ListNode* head, int k) {
-        int length = findLength(head);
+        ListNode* temp = head;
+        ListNode* p1 = NULL;
+        ListNode* p2 = NULL;
 
-        int k_1 = k ;
-        ListNode* node1 = head;
-        while(k_1 >1){
-            node1 = node1->next;
-            k_1--;
+        while(temp){
+            if(p2!=NULL){//tab activate hoga jab p1 milega
+                p2=p2->next;
+            }
+            k--;
+            if(k==0){
+                p1=temp;
+                p2=head;
+            }
+            temp = temp->next;
         }
-        int k_2 = length -k +1 ;
-        ListNode* node2 = head;
-        while(k_2 >1){
-            node2 = node2->next;
-            k_2--;
-        }
-
-        swap(node1->val,node2->val);
+        swap(p1->val,p2->val);
         return head;
     }
 };
